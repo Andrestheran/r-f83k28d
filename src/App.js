01a93d246239e12d9e7b5a14c080da78dcbbc814
Andres <andrestheran@outlook.com>
2021-11-22 13:02:01 -0500

    testsok

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
-      </div>
-    );
-  }
+  
+  constructor(props){
+    super(props)
+    this.state = { value:0 }
+    this.clickHandler = this.clickHandler.bind(this)
 }
-
+clickHandler(){
+    console.log("prevState", this.state.value)
+    this.setState({ value: this.state.value + 1 })
+}
+render(){
+    return (
+        <div>
+        <button id ="inc" onClick= {this.clickHandler}>Incrementa</button>
+        <span className = "value">{this.state.value}</span>
+        </div>
+    )
+}
+}
 export default App;
